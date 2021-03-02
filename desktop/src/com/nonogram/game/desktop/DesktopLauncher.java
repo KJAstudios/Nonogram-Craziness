5eb39b5bdd557109ac207cbecc0f099b6e67dfa0
KJAstudios <kja@surewest.net>
2021-03-02 12:51:54 -0800

    Initial commit

---
+++ desktop/src/com/nonogram/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nonogram.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nonogram.game.NonogramMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NonogramMain(), config);
+	}
+}
